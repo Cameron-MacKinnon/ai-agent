0e03051f608d8c9c98c1bb95b56338994cbb45b4
Cameron-MacKinnon <cameronjmackinnon@icloud.com>
2026-03-22 15:34:48 +0800

    load env var and configure client

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
+import os
+
+from dotenv import load_dotenv
+from google import genai
+
+from constants import MODEL
+
+# attempt to load env vars from /.env
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+if api_key is None:
+    raise RuntimeError(
+        "API key not found, make sure your .env file "
+        "contains a variable in the following format:\n"
+        "GEMINI_API_KEY='your_api_key_here'"
+    )
+
+# init gemini client
+client = genai.Client(api_key=api_key)
+
+
 def main():
-    print("Hello from ai-agent!")
+    response = client.models.generate_content(
+        model=MODEL,
+        contents=(
+            "Why is Boot.dev such a great place to learn "
+            "backend development? Use one paragraph maximum."
+        ),
+    )
+    print(response.text)
 
 
 if __name__ == "__main__":
