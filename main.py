918c4b8066bcaa5264b8d10db6c938f0e4abe554
Cameron-MacKinnon <cameronjmackinnon@icloud.com>
2026-03-22 15:45:10 +0800

    add response validation

--- main.py
+++ main.py
@@ -20,14 +20,26 @@ client = genai.Client(api_key=api_key)
 
 
 def main():
+    prompt = (
+        "Why is Boot.dev such a great place to learn "
+        "backend development? Use one paragraph maximum."
+    )
     response = client.models.generate_content(
         model=MODEL,
-        contents=(
-            "Why is Boot.dev such a great place to learn "
-            "backend development? Use one paragraph maximum."
-        ),
+        contents=prompt,
     )
-    print(response.text)
+
+    # validate response
+    if response.usage_metadata is None:
+        raise RuntimeError(
+            "usage metadata not found, it's likely that "
+            "the API request failed, try again later."
+        )
+
+    print(f"User prompt: {prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response:\n{response.text}")
 
 
 if __name__ == "__main__":
